0dd7012d94ccf05ac2827a10cad3681a4a5ed9e6
chris-reeves1 <dr_gonzo19@hotmail.com>
2024-06-20 09:41:57 +0000

    2

--- files/files-lab.py
+++ files/files-lab.py
@@ -6,15 +6,17 @@ sales = []
 with open("output.csv") as csvfile:
     reader = csv.reader(csvfile)
     next(reader)
-    for loop:
-        append companies top companies
-        append sales to sales: cast to int, remove commas(replace(",", ""))
+    for row in reader:
+        companies.append(row[0])
+        sales.append([int(x.replace(",", "")) for x in row[1: ]])
 
-monthly_sum = [expression-sum(x) item for x in iterable zip(*sales)] 
+monthly_sum = [sum(x) for x in zip(*sales)]
 
-yearly_sum {} = 
-for loop for companies
-    yearlysum[i] = sum(sales[i])
+yearly_sum = {} 
+for i in range(len(companies)):
+    yearly_sum[companies[i]] = sum(sales[i])
 
-print
-for loo .items()
+print(monthly_sum)
+print("yearly sales: ")
+for company, sales in yearly_sum.items():
+    print(company, sales)
---
+++ other/feedback.py
@@ -0,0 +1,34 @@
+# final feedback should be in this format:
+
+General comments
+Steve did well with ........efvbfvevevefvev
+efvevefvvfv
+verefvefvevefvev
+vefvefvefvefvefvvf
+
+Learner punctuality and engagement
+steve enaged wll ans was alesya iuctiula
+
+Recommendations on further learning
+have a look at advanced python such aas recursion. 
+
+requirments:
+type in all student names as an input
+prompt foe each student to rate them 1-4 in the following areas::
+- understanding level
+- contribution level
+- lab completeion level
+- punctuality level
+- engagement level
+- further_learning level
+
+map
+a general template of the feedback and a map to scores/descriptive words/phrases. 
+
+output 
+- individual file for each student with feedback in it
+
+optional (not windows)
+- open the file for manual editing
+
+save to folder with file name constructed form student name. 
